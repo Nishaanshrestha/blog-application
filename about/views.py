f55169e18fb7a2a9aee708d0806e56d05c1e8d08
Nishaan Shrestha <shresthanishan007@gmail.com>
2024-02-08 16:56:26 +0545

    update

--- about/views.py
+++ about/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(request):
-    return render(request,'about.html',{'number':2})
+    return render(request,'about.html',{'number':[2,2,7]})
